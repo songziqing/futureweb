b4581204d1193e895dd19b39cd5d2a8e1bc0c19e
songziqing <1044649609@qq.com>
2016-01-28 15:55:53 +0800

    又进行了一次上传，还是国坤的代码

---
+++ FutureGroup/Application/Home/Controller/IndexController.class.php
@@ -0,0 +1,29 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+class IndexController extends Controller {
+    public function index(){
+        $this -> display();
+    }
+
+    public function about(){
+        $this -> display();
+    }
+
+    public function service(){
+        $this -> display();
+    }
+
+    public function solution(){
+        $this -> display();
+    }
+
+    public function project(){
+        $this -> display();
+    }
+
+    public function contact(){
+        $this -> display();
+    }
+
+}
